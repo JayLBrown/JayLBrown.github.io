69443a7beba5a5760bf782c762c8ae577d473b5d
Jay Brown <jaquanlbrown@outlook.com>
2021-04-24 19:46:52 -0400

    l

---
+++ portfolio/js/main.js
@@ -0,0 +1,8 @@
+
+window.formbutton = window.formbutton|| 
+function(){(formbutton.q=formbutton.q||[]).push(arguments)};
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-4798EV29K8');
